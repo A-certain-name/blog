7bc61269f7524599efbe71a1a8baf7252dc3de8c
A-certain-name <fgc62577@psoxs.com>
2017-02-09 22:28:15 +0900

    Implement the database

--- work4/app.py
+++ work4/app.py
@@ -1,21 +1,60 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template, request, redirect, url_for
-import numpy as np
+import sqlite3
+from flask import Flask, render_template, request, session, g, redirect, url_for,abort,flash
+# import numpy as np
+# from __future__ import with_statement
+from contextlib import closing
+
+DATABASE = 'flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'root'
+PASSWORD = 'root'
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
-# Routing
+# DB接続
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+# DBの初期化
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+# リクエスト前処理
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+# リクエスト後処理
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+# ルーティング設定
+# トップページ
 @app.route('/')
 def index():
+    # 降順でテキストを取り出す
+    cur = g.db.execute('select title, text from entries order by id desc')
+    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
     title = "blogサイト"
     message = "今の気分を一言"
     return render_template('index.html',
-                           message=message, title=title)
+                           message=message, title=title, entries=entries)
 
+# 記事投稿
 @app.route('/post', methods=['POST', 'GET'])
 def post():
-    title = "投稿しました"
+    title = '投稿ページ'
     if request.method == 'POST':
         tweet = request.form['tweet']
         return render_template('index.html',
